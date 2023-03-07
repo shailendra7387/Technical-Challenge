26045ed9c27419afe9ecfbe75601dbb213520619
shailendra7387 <shailendra7387@gmail.com>
2023-03-07 17:32:39 +0530

    Update nested_object.py

--- 03_NestedObjectQuery/nested_object.py
+++ 03_NestedObjectQuery/nested_object.py
@@ -1,9 +1,8 @@
-
-
 def getKey(obj: dict):
     keys = list(obj)
+ 
     if len(keys) != 1:
-        raise Exception('Either multiple keys or empty dict found')
+        raise Exception('empty dict found')
     else:
         return keys[0]
 
@@ -14,9 +13,13 @@ def getNestedValue(obj: dict, key: str, isFound = False):
     if (isFound or (key in obj.keys())) :
         if type(obj[key]) is dict:
             return getNestedValue(obj[key], getKey(obj[key]), True)
-        else:            
+        else:
             return obj[getKey(obj)]
     else:
         nestedKey = getKey(obj)
         return getNestedValue(obj[nestedKey], key, False)
 
+if __name__ == '__main__':
+    obj = {'x': {'y': {'z': 'a'}}}  
+    value = getNestedValue(obj, 'y')
+    print(value)
